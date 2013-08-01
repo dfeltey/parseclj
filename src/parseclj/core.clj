aa9ed64ac4e90dafd6e472341a685882ab5316bd
Dan Feltey <dfeltey@gmail.com>
2013-08-01 10:56:05 -0700

    added <*> and <|> combinators, abstracted out curry pattern...

--- src/parseclj/core.clj
+++ src/parseclj/core.clj
@@ -1,6 +1,14 @@
 (ns parseclj.core)
 
 
+;; helper function to hide the exception handling
+;; that seems to be needed in the definition of <*>
+(defn curry [f x]
+  (try
+     (f x)
+     (catch Exception _ (partial f x))))
+
+
 
 ;; Basic symbol combinator for parsing a single symbol
 ;; if it encounters something that is not a sequence
@@ -27,19 +35,35 @@
 
 
 ;; traditional Haskell-ish sequencing combinator
+;; need to generalize it to take more than 2 argumnents
+;; otherwise syntax will get messy very quickly
+;; the challenge then becomes handling parsers that ignore results...
+;; maybe macros can fix that issue...?
 (defn <*> [p1 p2]
   (fn [inp]
       (for [[v1 ss1] (p1 inp)
 	    [v2 ss2] (p2 ss1)]
-	      (try
-		   [(v1 v2) ss2]
-		   (catch Exception _ [(partial v1 v2) ss2])))))
-                ;; the lack of automatic currying makes this part challengin
-                ;; need to somehow reflect on the number of arguments required
-                ;; or just always use partial....
+		   [(curry v1 v2) ss2])))
+                   ;; curry does exception handling to deal with not providing
+                   ;; v1 with all of its arguments immediately
+
+
+(defn <|> [p1 p2 & ps]
+  (fn [inp]
+    (let [result (concat (p1 inp) (p2 inp))]
+      (cond
+        (empty? ps) result
+        :else (reduce <*> result ps)))))
+    
+    
 
 
 
+
+
+
+
+;; simple tests, need to move these out of this file
 (def pLettera (pSym \a))
 
 (def pString_aa
